57e7d5abb5b01605a1f1494a4ab9420a3826e687
01xniel <dnl.dzbn@gmail.com>
2024-12-14 16:35:57 +0200

    minor change

--- app/src/main/java/com/example/kpi_mobileappdev_lab3/navigation/AppRoutes.kt
+++ app/src/main/java/com/example/kpi_mobileappdev_lab3/navigation/AppRoutes.kt
@@ -3,4 +3,5 @@ package com.example.kpi_mobileappdev_lab3.navigation
 // усі існуючі маршрути застосунку
 
 const val CalcInputRoute = "input"
-const val CalcResultRoute = "result/{profit}/{electricityNoImbalance}/{penalty}/{electricityImbalance}/{netProfit}"
+const val CalcResultRoute = "result/{profit}/{electricityNoImbalance}/{penalty}" +
+        "/{electricityImbalance}/{netProfit}"
